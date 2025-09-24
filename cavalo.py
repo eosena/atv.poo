b05cabc20918afbfd5afc0fd52c06c399c874fbf
eosena <145704116+eosena@users.noreply.github.com>
2025-09-24 20:10:27 -0300

    Add files via upload

---
+++ cavalo.py
@@ -0,0 +1,6 @@
+class Cavalo():
+    def comer(comida):
+        print(f"A comida {comida} está deliciosa!")
+
+    def dormir(horas):
+        print(f"O cavalo dormiu por {horas} horas!")
---
+++ main.py
@@ -0,0 +1,5 @@
+from cavalo import Cavalo
+
+Cavalo.comer("grama")
+print()
+Cavalo.dormir(6)
