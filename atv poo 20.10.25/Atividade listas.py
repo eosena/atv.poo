bb2fbfe5cbad51bdafffa1654b8215907f29118a
eosena <145704116+eosena@users.noreply.github.com>
2025-10-20 20:54:45 -0300

    Add files via upload

---
+++ atv poo 20.10.25/Atividade listas.py
@@ -0,0 +1,19 @@
+#QUESTÃO 1
+
+lista1 = [1,2,3,4,5]
+print(lista1)
+
+#QUESTÃO 2
+print(lista1[0], lista1[2], lista1[4])
+
+#QUESTÃO 3
+lista1[3] = 5
+print(lista1)
+#QUESTÃO 4
+a = int(input("Insira um número para adicionar à lista: "))
+lista1.append(a)
+print(lista1)
+
+#QUESTÃO 5
+lista1.pop(2)
+print(lista1)
